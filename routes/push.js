bec0f3f8304db083c04464b8d156283a00a9222d
AleXxxj <alexxxjahn@gmail.com>
2026-07-15 02:20:27 +0100

    included_segments

--- routes/push.js
+++ routes/push.js
@@ -12,7 +12,7 @@ router.post('/send', auth, async (req, res) => {
 
     const payload = {
       app_id: process.env.ONESIGNAL_APP_ID,
-      included_segments: ['Subscribed Users'],
+      included_segments: ['Total Subscriptions'],
       headings: { en: title },
       contents: { en: message },
     };
